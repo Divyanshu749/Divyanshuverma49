008c1ef10bca8533c1584e88fbc8af1fb774f968
Divyanshu749 <103191165+Divyanshu749@users.noreply.github.com>
2022-05-12 22:17:06 +0530

    Add files via upload

---
+++ Assignment 3 sol .py
@@ -0,0 +1,54 @@
+"C:\Users\Divyanshu Verma\PycharmProjects\pythonProject4\venv\Scripts\python.exe" "C:/Users/Divyanshu Verma/PycharmProjects/pythonProject4/main.py"
+Ans-1 :-
+Enter a number: 49
+Binary equivalent of 49 is 110001
+Ans-2 :-
+Enter expression: 497*749^115*597
+497*749^115*597 = 303666
+Ans-3:-
+a) (3+4)(5) =  35
+Enter an integer = 49
+b) 1176.0
+Enter radius =14
+c) 2463.0086404143976
+Enter angle 1 = 59
+Enter angle 2 = 65
+d) 1.2493825604670616
+Enter x coordinate of first point: 7
+Enter y coordinate of first point: 9
+Enter x coordinate of second point: 4
+Enter y coordinate of second point: 15
+e) -2.0
+Ans-4 :-
+4.(a)
+0
+1
+2
+3
+4
+4.(b)
+3
+4
+5
+6
+7
+8
+9
+4.(c)
+4
+7
+10
+4.(d)
+15
+13
+11
+9
+7
+4.(e)
+Ans-5:-
+Enter number of carbon atoms in molecule: 12
+Enter number of hydrogen atoms in molecule: 8
+Enter number of oxygen atoms in molecule: 16
+The molecular weight of the compound is 408.18232
+
+Process finished with exit code 0
