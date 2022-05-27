5de83892c3740a4bfa864d565f482f45ba11bdc4
Divyanshu749 <103191165+Divyanshu749@users.noreply.github.com>
2022-05-27 22:49:49 +0530

    Add files via upload

---
+++ Assignment_4_soln.py
@@ -0,0 +1,21 @@
+Question 1:7x3=21
+Right!
+Question 2:5x8=58
+Wrong. The correct answer is 40
+Question 3:3x6=122
+Wrong. The correct answer is 18
+Question 4:2x5=32
+Wrong. The correct answer is 10
+Question 5:4x2=215
+Wrong. The correct answer is 8
+Question 6:9x7=21
+Wrong. The correct answer is 63
+Question 7:4x4=16
+Right!
+Question 8:0x1=21
+Wrong. The correct answer is 0
+Question 9:6x1=6
+Right!
+Question 10:5x5=25
+Right!
+>
