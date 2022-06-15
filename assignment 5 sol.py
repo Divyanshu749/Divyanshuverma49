e58aeda63e661c97998b599f1ba5d551c24dc2ad
Divyanshu749 <103191165+Divyanshu749@users.noreply.github.com>
2022-06-15 23:59:29 +0530

    Add files via upload

---
+++ assignment 5 sol.py
@@ -0,0 +1,78 @@
+"C:\Users\Divyanshu Verma\PycharmProjects\pythonProject9\venv\Scripts\python.exe" "C:/Users/Divyanshu Verma/PycharmProjects/pythonProject9/main.py"
+ The original string is :  Python is interesting.
+ The reversed string is : .gnitseretni si nohtyP
+ Enter the lower range limit: 7
+ Enter the upper range limit: 49
+ Enter the number to be divided by: 12
+12
+24
+36
+48
+Enter the first side:12
+enter the second side:7
+enter the third side:5
+triangle cannot be formed with these sides
+
+*
+**
+***
+****
+*****
+****
+***
+**
+*
+Enter Right Triangle Consecutive Alphabets Rows = 5
+====The Right Triangle of Consecutive Alphabets Pattern====
+A
+B B
+C C C
+D D D D
+E E E E E
+Please, Enter the Lowest Range Value: 0
+Please, Enter the Upper Range Value: 100
+The Prime Numbers in the range are:
+2
+3
+5
+7
+11
+13
+17
+19
+23
+29
+31
+37
+41
+43
+47
+53
+59
+61
+67
+71
+73
+79
+83
+89
+97
+77,154,231,308,385,462
+ Enter the numbers: 0
+ Enter the numbers: 2
+ Enter the numbers: 1
+ Enter the numbers: 6
+ Enter the numbers: 5
+ Enter the numbers: 4
+ Enter the numbers: 8
+ Enter the numbers: 2
+ Enter the numbers: 6
+ Enter the numbers: 7
+[0, 1, 2, 3, 4, 5, 6, 7, 8, 9]
+[]
+[0, 2, 4, 6, 8]
+[1, 3, 5, 7, 9]
+{0: 1, 2: 2, 1: 1, 6: 2, 5: 1, 4: 1, 8: 1, 7: 1}
+{'Python': 1, 'is': 1, 'a': 1, 'case': 1, 'sensitive': 1, 'language.': 1}
+
+Process finished with exit code 0
