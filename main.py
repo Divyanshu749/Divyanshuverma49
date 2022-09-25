e27c2a597a13b51317c30949159f14e98fe01ed0
Divyanshu749 <103191165+Divyanshu749@users.noreply.github.com>
2022-09-25 23:18:03 +0530

    Add files via upload

---
+++ main.py
@@ -0,0 +1,44 @@
+Enter the no of family members:
+
+4
+
+Enter the name of family member:
+
+Div
+
+Enter the age of family member:
+
+19
+
+Enter the name of family member:
+
+yash
+
+Enter the age of family member:
+
+52
+
+Enter the name of family member:
+
+Soumik
+
+Enter the age of family member:
+
+20
+
+Enter the name of family member:
+
+20
+
+Enter the age of family member:
+
+21
+
+|Name:  yash  age:  52 |  ---><------ |Name:  20  age:  21 |  ---><------ |Name:  Soumik  age:  20 |  ---><------ |Name:  Div   age:  19 |  ---><------ None
+
+
+
+** Process exited - Return Code: 0 **
+
+
+Press Enter to exit terminal
